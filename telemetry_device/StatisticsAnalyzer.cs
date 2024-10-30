eb69b823202d4561c687f03966c8c6c9112f04b0
Tomer <tomer.cohen243@gmail.com>
2024-10-30 14:23:38 +0200

    [wip] started statics analyzer

---
+++ telemetry_device/StatisticsAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telemetry_device
+{
+    class StatisticsAnalyzer
+    {
+        private static StatisticsAnalyzer _instance;
+
+        private StatisticsAnalyzer()
+        {
+        }
+        public static StatisticsAnalyzer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new StatisticsAnalyzer();
+                }
+                return _instance;
+            }
+        }
+    }
+}
